28198608063920118b8daef21a9bf707f3c468bd
dishonjp <dishonjp11@gmail.com>
2019-12-27 18:02:00 +0530

    [Dishon]added comments and conditions

--- functional/quadratic/quadratic.js
+++ functional/quadratic/quadratic.js
@@ -1,11 +1,28 @@
-const readline=require('readline-sync');
-const access=require('./quadraticBL');
-let a,b,c;
+/**
+ * Execution   : default node    cmd>node.quadratic.js
+ * 
+ * purpose     : To find root for given equation
+ * 
+ * 
+ * @description :to find the roots of the equation a*x*x + b*x + c.
+ * @file        :quadratic.js
+ * @overview    :first finding the value for both x and using those values in equation
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :22-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./quadraticBL');
+let a, b, c;
 console.log('to find quadratic eqn');
-a=readline.questionInt('Enter the value for a :');
-b=readline.questionInt('Enter the value for b :');
-c=readline.questionInt('Enter the value for c :');
-let delta=access.toFindDelta(a,b,c)
-let x1=access.toFindRoot1(delta,a,b);
-let x2=access.toFindRoot2(delta,a,b);
-console.log(access.toFindEquation(x1,x2,a,b,c));
+a = readline.question('Enter the value for a :');
+b = readline.question('Enter the value for b :');
+c = readline.question('Enter the value for c :');
+if (a === '' && b === '' && c === '') {
+    console.log('Give valid input');
+} else {
+    let delta = access.toFindDelta(a, b, c)
+    let x1 = access.toFindRoot1(delta, a, b);
+    let x2 = access.toFindRoot2(delta, a, b);
+    console.log(access.toFindEquation(x1, x2, a, b, c));
+}
--- functional/quadratic/quadraticBL.js
+++ functional/quadratic/quadraticBL.js
@@ -1,63 +1,70 @@
-
-
-    exports.toFindDelta=(a,b,c)=>{
-        try {
-        let delta=(b*b)-(4*a*c);
+/**
+ * @module(number,number,number)a,b,c-user input
+ */
+exports.toFindDelta = (a, b, c) => {
+    try {
+        let delta = (b * b) - (4 * a * c); //mathematical calculation to find delta
         console.log(delta);
-        
-        if (isNaN(delta)) {
+
+        if (isNaN(delta)) { //checking whether the number is not a number
             return NaN;
         } else {
-            return delta;
+            return delta; //returning delta value
         }
-        }
-        catch (error) {
-            console.log(error);
-            
-        }
-        
+    } catch (error) {
+        console.log(error);
+
     }
-    exports.toFindRoot1=(delta,a,b)=>{
-        try {
-            let x1=(-b+Math.sqrt(delta))/(2*a);
-        console.log("value of x1",x1);
+
+}
+/**
+ * @module(number,number,number)delta,a,b - is used in equation
+ */
+exports.toFindRoot1 = (delta, a, b) => {
+    try {
+        let x1 = (-b + Math.sqrt(delta)) / (2 * a); //to find the root of first x
+        console.log("value of x1", x1);
         if (isNaN(x1)) {
             return NaN;
         } else {
-            return x1;
+            return x1; //returning first root x
         }
-        } catch (error) {
-            console.log(error);
-            
-        }
-        
+    } catch (error) {
+        console.log(error);
+
     }
-    exports.toFindRoot2=(delta,a,b)=>{
-        try {
-            let x2=(-b-Math.sqrt(delta))/(2*a);
+
+}
+/**
+ * @module(number,number,number)delta,a,b - is used in equation
+ */
+exports.toFindRoot2 = (delta, a, b) => {
+    try {
+        let x2 = (-b - Math.sqrt(delta)) / (2 * a); //to find the root of second x
         if (isNaN(x2)) {
             return NaN;
         } else {
-            return x2;
+            return x2; //returning second root x
         }
-        } catch (error) {
-            console.log(error);
-            
-        }
-        
+    } catch (error) {
+        console.log(error);
+
     }
-    exports.toFindEquation=(x1,x2,a,b,c)=>{
-        try {
-            let equation= a*x1*x2+b*x1+c;
-        if (isNaN(equation)) 
-        {
+}
+/**
+ * @module(number,number,number,number)x1,x2,a,b,c - used in equation
+ */
+exports.toFindEquation = (x1, x2, a, b, c) => {
+    try {
+        let equation = a * x1 * x2 + b * x1 + c; //just implementing the values
+        if (isNaN(equation)) {
             return NaN;
         } else {
-            return equation;
+            return equation; //returning final value
         }
-        } catch (error) {
-            console.log(error);
-            
-        }
-        
+    } catch (error) {
+        console.log(error);
+
     }
+
+}
