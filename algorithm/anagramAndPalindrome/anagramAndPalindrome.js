58135a59a680a7ccad0101f93f8e7dbdfc0507ea
dishonjp <dishonjp11@gmail.com>
2019-12-27 19:08:02 +0530

    [Dishon]added commit to every function

--- algorithm/anagramAndPalindrome/anagramAndPalindrome.js
+++ algorithm/anagramAndPalindrome/anagramAndPalindrome.js
@@ -1,14 +1,25 @@
-const access=require('./anagramAndPalindromeBL');
+/**
+ * Execution   : default node    cmd>node.anagramAndPalindrome.js
+ * 
+ * purpose     : To find prime no and anagram and palindrome in range 1 to 1000
+ * 
+ * 
+ * @description :To find prime no and anagram and palindrome in range 1 to 1000
+ * @file        :anagramAndPalindrome.js
+ * @overview    :Generate values and check whether it is prime no and anagram and palindrome
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :26-12-2019
+ * */
+const access = require('./anagramAndPalindromeBL');
 for (let i = 2; i <= 1000; i++) {
-    let primeNumber=access.toFindPrimeNumber(i);
+    let primeNumber = access.toFindPrimeNumber(i);
     if (primeNumber) {
-      //console.log(i+ ' is PrimeNumber');
-       let isPalindrome=access.toFindPalindrome(i);
+        console.log(i + ' is PrimeNumber');
+        let isPalindrome = access.toFindPalindrome(i);
         if (isPalindrome) {
-            //console.log(i+ ' is Palindrome');    
+            console.log(i + ' is Palindrome');
         }
         access.toFindAnagram(i);
-       
-    }    
+    }
 }
-
