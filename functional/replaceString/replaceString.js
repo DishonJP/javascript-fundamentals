b34b02029c38063c7da82b257d618495658eb13a
dishonjp <dishonjp11@gmail.com>
2019-12-27 16:27:32 +0530

    [Dishon]added comments and conditions

--- functional/replaceString/replaceString.js
+++ functional/replaceString/replaceString.js
@@ -1,5 +1,24 @@
-let readline=require('readline-sync');
-const access=require('./replaceStringBL');
-let name=readline.question('Enter your name: ');
-
-console.log(access.replaceString(name));
+/**
+ * Execution   : default node    cmd>node.replaceString.js
+ * 
+ * purpose     : replace a string
+ * 
+ * 
+ * @description :replace a given string with user given input
+ * @file        :replaceString.js
+ * @overview    :to replace current string with user given string
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :20-12-2019
+ * */
+let readline = require('readline-sync');
+const access = require('./replaceStringBL');
+let name = readline.question('Enter your name: ');
+if (name === '') {
+    console.log('Give valid input');
+} else {
+    let replacedString = access.replaceString(name);
+    if (replacedString !== '') {
+        console.log(replacedString);
+    }
+}
--- functional/replaceString/replaceStringBL.js
+++ functional/replaceString/replaceStringBL.js
@@ -1,11 +1,18 @@
+/**
+ * @module(String)name-user given input
+ */
+exports.replaceString = (name) => {
+    try {
+        if (name.length > 2) { //checking length of user string
+            let sentence = 'Hello username,How are you';
+            let replacedSentence = sentence.replace('username', name); //replacing current string with user string
+            return replacedSentence; //returning value
+        } else {
+            console.log('String should have minimum of 3 character');
+            return ''; //retuning empty string for validation purpose
+        }
+    } catch (error) {
+        console.log(error);
 
-try {
-    exports.replaceString=(name)=>{
-        let sentence='Hello username,How are you';
-    let replacedSentence=sentence.replace('username',name);
-    return replacedSentence;
     }
-} catch (error) {
-    console.log(error);
-    
 }
