fb85e20652533877691f93cc1917b102250a936a
dishonjp <dishonjp11@gmail.com>
2019-12-27 11:34:09 +0530

    [Dihson]Added comments

--- algorithm/binarySearch/binarySearch.js
+++ algorithm/binarySearch/binarySearch.js
@@ -1,9 +1,28 @@
-let fs=require('fs');
-var contents=fs.readFileSync('demo.txt','utf8');
-let replacedContents=contents.replace(',',' ');
-let arr=new Array(replacedContents.length);
-for (let i = 0; i < arr.length; i++) {
-    arr[i]=replacedContents[i];
+/**
+ * Execution   : default node    cmd>node.binarySearch.js
+ * 
+ * purpose     : To find a word from a file
+ * 
+ * 
+ * @description
+ * 
+ * @file        :binarySearch.js
+ * @overview    :To find a word given by user from a file
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :27-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./binarySearchBL');
+let fs = require('fs');
+var contents = fs.readFileSync('demo.txt', 'utf8');
+let replacedContents = contents.replace(',', ' ');
+let splitString = replacedContents.split(' ');
+splitString.sort();
+let input = readline.question('Enter a word to search in a file : ')
+let isDataPresent = access.binarySearch(splitString, input);
+if (isDataPresent > -1) {
+    console.log('Word is present in a file');
+} else {
+    console.log('Word is not present in a file');
 }
-console.log(arr);
-
---
+++ algorithm/binarySearch/binarySearchBL.js
@@ -0,0 +1,26 @@
+/**
+ * @module(sortArray, input) //array of string , string
+ * 
+ */
+exports.binarySearch = (sortArray, input) => {
+    try {
+        let front = 0, //to pointing to first index
+            end = sortArray.length - 1; //to pointing to last index
+        let mid = Math.floor((front + end) / 2); //to pointing to middle index
+        console.log(sortArray);
+        while (front <= end && sortArray[mid] != input) { //to check the string is present in that specific index
+            if (input < sortArray[mid]) { //to check whether the value is present left from mid index
+                end = mid - 1;
+            } else {
+                front = mid + 1;
+            }
+            mid = Math.floor((front + end) / 2);
+        }
+        console.log(mid);
+        return (sortArray[mid] != input) ? -1 : mid + 1; //to check whether the word is present or not
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
