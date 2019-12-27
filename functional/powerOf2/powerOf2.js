93bea24a61eeb153bac0b89c4f48bc8d681ef824
dishonjp <dishonjp11@gmail.com>
2019-12-27 16:34:37 +0530

    [Dishon]added comments and conditions

--- functional/powerOf2/powerOf2.js
+++ functional/powerOf2/powerOf2.js
@@ -1,5 +1,22 @@
-const readline=require('readline-sync');
-const access=require('./powerOf2BL');
-let nthNumber=readline.questionInt('To find 2 power n ,enter nth value');
-access.powerOf2(nthNumber);
+/**
+ * Execution   : default node    cmd>node.powerOf2.js
+ * 
+ * purpose     : To find power of 2
+ * 
+ * 
+ * @description :This program takes a command-line argument N and prints a table of the powers of 2 that are less than or equal to 2^N.
+ * @file        :powerOf2.js
+ * @overview    :Generate nth power of 2
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :20-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./powerOf2BL');
+let nthNumber = readline.question('To find 2 power n ,enter nth value');
+if (nthNumber === '') {
+    console.log('Enter valid input');
 
+} else {
+    access.powerOf2(nthNumber);
+}
--- functional/powerOf2/powerOf2BL.js
+++ functional/powerOf2/powerOf2BL.js
@@ -1,15 +1,18 @@
-try {
-    exports.powerOf2=(nthNumber)=>{
-        let i=0;if (nthNumber>0) {
-            while (i<=nthNumber) {
-                console.log('2 power '+i+' is '+Math.pow(2,i));
+/**
+ * @module(number)nthnumber-user input
+ */
+exports.powerOf2 = (nthNumber) => {
+    try {
+        let i = 0;
+        if (nthNumber > 0) { //to check user input should be greater than 0
+            while (i <= nthNumber) {
+                console.log('2 power ' + i + ' is ' + Math.pow(2, i)); //calculating value
                 i++;
             }
         } else {
             console.log('value should be greater than 0');
         }
-        
+    } catch (error) {
+        console.log(error);
     }
-} catch (error) {
-    console.log(error);
 }
