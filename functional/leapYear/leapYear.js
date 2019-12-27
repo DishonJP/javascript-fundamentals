2939ba45a521603948ad46e8b5aea46fa64ef6a2
dishonjp <dishonjp11@gmail.com>
2019-12-27 17:24:43 +0530

    [Dishon]added comments and conditions

--- functional/leapYear/leapYear.js
+++ functional/leapYear/leapYear.js
@@ -1,4 +1,20 @@
-const readline=require('readline-sync');
-const access=require('./leapYearBL');
-let year=readline.questionInt('Enter the year to check whether its a leap year or not ');
-console.log(access.toCheckYear(year));
+/**
+ * Execution   : default node    cmd>node.leapYear.js
+ * 
+ * purpose     : To find coupon number
+ * 
+ * 
+ * @description :To find the user enter year is leap year or not.
+ * @file        :leapYear.js
+ * @overview    :Generate n random numbers and find the user entered number
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :20-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./leapYearBL');
+let year = readline.question('Enter the year to check whether its a leap year or not ');
+if (year === '') {
+    console.log('Give valid input');
+} else
+    console.log(access.toCheckYear(year));
--- functional/leapYear/leapYearBL.js
+++ functional/leapYear/leapYearBL.js
@@ -1,16 +1,18 @@
-
-try {
-    exports.toCheckYear=(year)=>{
+/**
+ * @module(number)year-user input
+ */
+exports.toCheckYear = (year) => {
+    try {
         let leapYear;
-        if (year.toString().length===4) {
-             leapYear=year%400===0 || (year%100!==0 && year%4===0)?'Leap year':'Not a Leap Year';
+        if (year.toString().length === 4) {//checking length of year
+            leapYear = year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0) ? 'Leap year' : 'Not a Leap Year';//checking whether it is leap year or not
         } else {
             return 'Year should be of 4 digit';
-            
+
         }
         return leapYear;
+    } catch (error) {
+        console.log(error);
+
     }
-} catch (error) {
-    console.log(error);
-    
 }
