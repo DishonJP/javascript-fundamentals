52fd57343f47b6b24175cbbecae4e7d85c5efc83
dishonjp <dishonjp11@gmail.com>
2019-12-27 17:42:17 +0530

    [Dishon]added comments and conditions

--- functional/primeFactor/primeFactor.js
+++ functional/primeFactor/primeFactor.js
@@ -1,16 +1,21 @@
 /**
  * Execution   : default node    cmd>node.primeFactor.js
  * 
- * purpose     : To find Prime Factorization
+ * purpose     : To find prime factor
  * 
  * 
- * @description
- * 
+ * @description :Computes the prime factorization of N using brute force.
  * @file        :primeFactor.js
- * @overview    :to generate Prime factor for a given value
- * 
+ * @overview    :user will give an input and must need to find the prime factor of that number
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :20-12-2019
  * */
-let readline=require('readline-sync');
-let nthno=readline.questionInt('Enter the value to find Prime Factorization');
-let util=require('./primeFactorBL');
-util.primeFactor(nthno);
+let readline = require('readline-sync');
+let nthno = readline.question('Enter the value to find Prime Factorization');
+let util = require('./primeFactorBL');
+if (nthno === '') {
+    console.log('Give valid input');
+
+} else
+    util.primeFactor(nthno);
--- functional/primeFactor/primeFactorBL.js
+++ functional/primeFactor/primeFactorBL.js
@@ -1,70 +1,49 @@
 /**
- *module.exports.primeFactor=(nthNo)=>{ @param {number} nthNo - value
- {
-     @param {loop}              -to loop till nth number
-     @return {boolean} primeno  -generates prime number from 2 to nth number
-     @param {condition}  if     -to filter prime numbers
-     {
-        @param {number,number} (index,nthno)    -        
-     }
-     blend: function findPrimeNo(index) {
-         @param {loop}          -to loop from 2 to n/2
-         {
-             @param {condition} - to check a number is prime no or not
-             @return {boolean}
-         }
-     }
-     darker: function isFactor(index,nthno){
-        @param {loop}           -to check nth value not equal to 0
-        {
-            @param {condition} if  -to check nth number is divisible by prime number
-            {
-                @return {number} index -to print the value
-            }
-            @param {condition}  else- to break the loop
-        }
-     }
- }  
- **/exports.primeFactor=(nthno)=>{
+ * @module(number)nthNO-user input
+ */
+exports.primeFactor = (nthno) => {
     try {
-        console.log('Prime Factor of '+nthno+' are ');
-    for (let index = 2; index <=nthno; index++) {
-        let primeno=findPrimeNo(index);
-        if (primeno) {
-            //console.log(index);
-            isFactor(index,nthno);
+        console.log('Prime Factor of ' + nthno + ' are ');
+        for (let index = 2; index <= nthno; index++) { //for looping till user entered number
+            let primeno = findPrimeNo(index); //calling function
+            if (primeno) { //to check it is prime number or not
+                //console.log(index);
+                isFactor(index, nthno); //calling function
+            }
         }
-    }
-    function findPrimeNo(index) 
-    {
-     let i=2;
-     while (i<=index/2) 
-     {
-         if (index%i!=0) {
-             i++;
-         }
-         else
-         return false;
-     }   
-     return true;
-    }
-    function isFactor(index,nthno) 
-    {
-        while (nthno!=0) 
-        {
-           if (nthno%index==0) 
-           {
-               nthno=nthno/index;
-               console.log(index);
-           }
-           else
-           {
-               break;
-           } 
+        /**
+         * 
+         * @param {number} index -to check index is prime or not
+         */
+        function findPrimeNo(index) {
+            let i = 2;
+            while (i <= index / 2) //looping till index by half
+            {
+                if (index % i != 0) { //for filtering prime number
+                    i++;
+                } else
+                    return false; //if index mod i zero means its not a prime number
+            }
+            return true; //means index is prime number
         }
-    }      
-    } catch (error) 
-    {
-        console.log(error);    
+        /**
+         * 
+         * @param {number} index - value from loop
+         * @param {number} nthno -value from user
+         */
+        function isFactor(index, nthno) {
+            while (nthno != 0) //to make loop run till the user input reaches 0
+            {
+                if (nthno % index == 0) //to check user input mod with any prime number returns 0
+                {
+                    nthno = nthno / index; //this mathematical calculation helps to find prime factors
+                    console.log(index);
+                } else {
+                    break;
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
