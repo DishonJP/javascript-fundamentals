12430f5110924475b6e79a93e7d570f5517b9a61
dishonjp <dishonjp11@gmail.com>
2019-12-27 18:32:52 +0530

    [Dishon]added comments and conditions

--- functional/array2D/array2D.js
+++ functional/array2D/array2D.js
@@ -1,6 +1,23 @@
-const access=require('./array2DBL');
-const readline=require('readline-sync');
-let row=readline.questionInt('how many rows do you need :');
-let column=readline.questionInt('how many column do you need :');
-let intialArray=access.initializeArray(row,column);
-access.displayArray(intialArray);
+/**
+ * Execution   : default node    cmd>node.array2D.js
+ * 
+ * purpose     : to store and display any type of value
+ * 
+ * 
+ * @description :A library for reading in 2D arrays of integers, doubles, or booleans from standard input and printing them out to standard output.
+ * @file        :array2D.js
+ * @overview    :get row and column from user and generate a 2D array and store any type of values
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :23-12-2019
+ * */
+const access = require('./array2DBL');
+const readline = require('readline-sync');
+let row = readline.question('how many rows do you need :');
+let column = readline.question('how many column do you need :');
+if (row === '' && column === '') {
+
+} else {
+    let intialArray = access.initializeArray(row, column);
+    access.displayArray(intialArray);
+}
--- functional/array2D/array2DBL.js
+++ functional/array2D/array2DBL.js
@@ -1,30 +1,40 @@
-const readline=require('readline-sync');
-exports.initializeArray=(row,column)=>{
+/**
+ * @module(number,number)row,column - used for creating 2D Array
+ */
+const readline = require('readline-sync');
+exports.initializeArray = (row, column) => {
     try {
-        let arr=new Array(row);
-    for (var i = 0; i < arr.length; i++) { 
-        arr[i] = new Array(column); 
-        for (let j = 0; j < column; j++) {
-            arr[i][j]=(readline.question('enter the value for index:'+i+','+j+' : '));
-        }  
-    }
-    return arr;
-    } catch (error) {
-        console.log(error);
-        
-    }
-    
-}
-exports.displayArray=(intialArray)=>{
-try {
-    for (let i = 0; i < intialArray.length; i++) {
-        for (let j = 0; j < intialArray.length; j++) {
-            {
-                console.log(intialArray[i][j]);   
+        let arr = new Array(row);
+        for (let i = 0; i < row; i++) { //creating 2D array
+            arr[i] = new Array(column);
+        }
+        for (let i = 0; i < row; i++) {
+
+            for (let j = 0; j < column; j++) {
+                arr[i][j] = (readline.question('enter the value for index:' + i + ',' + j + ' : ')); //storing values in array
             }
         }
+
+        return arr; //returing array
+    } catch (error) {
+        console.log(error);
+
     }
-} catch (error) {
-    console.log(error);
+
 }
+/**
+ * @module(Array)-initialArray - to display the values present in the array
+ */
+exports.displayArray = (intialArray) => {
+    try {
+        for (let i = 0; i < intialArray.length; i++) { //helps to display the value from index to index
+            for (let j = 0; j < intialArray.length; j++) {
+                {
+                    console.log(intialArray[i][j]);
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
