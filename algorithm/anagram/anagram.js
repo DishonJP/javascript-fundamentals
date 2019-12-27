45e81c2ca678ec3419e394884e1e134bb754eac4
dishonjp <dishonjp11@gmail.com>
2019-12-27 18:50:13 +0530

    [Dishon]added comments and conditions

--- algorithm/anagram/anagram.js
+++ algorithm/anagram/anagram.js
@@ -1,6 +1,24 @@
-const readline=require('readline-sync');
-const access=require('./anagramBL');
+/**
+ * Execution   : default node    cmd>node.anagram.js
+ * 
+ * purpose     : To find any two strings are anagram
+ * 
+ * 
+ * @description :One string is an anagram of another if the second is simply a rearrangement of the first. For example, 'heart' and 'earth' are anagrams.
+ * @file        :anagram.js
+ * @overview    :get two strings as input from user and check whether it is anagram are not
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :23-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./anagramBL');
 console.log('Program to check anagram');
-let string1=readline.question('Enter the first String : ');
-let string2=readline.question('Enter the second String : ');
-access.toCheckAnagram(string1,string2);
+let string1 = readline.question('Enter the first String : ');
+let string2 = readline.question('Enter the second String : ');
+if (string1 === '' && string2 === '') {
+    console.log('Give proper input');
+
+} else {
+    access.toCheckAnagram(string1, string2);
+}
--- algorithm/anagram/anagramBL.js
+++ algorithm/anagram/anagramBL.js
@@ -1,29 +1,32 @@
-exports.toCheckAnagram=(string1,string2)=>{
-try {
-    string1=string1.split('');
-    string2=string2.split('');
-    if (string1.length===string2.length) {
-        let count=0;
-        for (let i = 0; i < string1.length; i++) {
-            for (let j = 0; j < string2.length; j++) {
-                if (string1[i]===string2[j]) {
-                    string2[j]=undefined;
-                    count++;
+/**
+ * @module(String,String)string1,string2 - used for finding anagram
+ */
+exports.toCheckAnagram = (string1, string2) => {
+    try {
+        string1 = string1.split(''); //spliting char by char
+        string2 = string2.split('');
+        if (string1.length === string2.length) { //comparing both string lengths
+            let count = 0;
+            for (let i = 0; i < string1.length; i++) {
+                for (let j = 0; j < string2.length; j++) {
+                    if (string1[i] === string2[j]) { //checking whether the char matches
+                        string2[j] = undefined; //to avoid duplicate char
+                        count++;
+                    }
                 }
-            }    
-        }
-        if (count===string1.length) {
-            console.log('Anagram');
-        }else{
+            }
+            if (count === string1.length) { //checking both strings are anagram
+                console.log('Anagram');
+            } else {
+                console.log('not a Anagram');
+            }
+        } else {
             console.log('not a Anagram');
+
         }
-    } else {
-        console.log('not a Anagram');
-        
+    } catch (error) {
+        console.log(error);
+
     }
-} catch (error) {
-    console.log(error);
-    
-}
-    
+
 }
