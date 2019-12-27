2bf2b00a708804e4228076960f16d77151e28979
dishonjp <dishonjp11@gmail.com>
2019-12-27 17:14:28 +0530

    [Dishon]added comments and conditions

--- functional/flipCoin/flipCoin.js
+++ functional/flipCoin/flipCoin.js
@@ -1,4 +1,20 @@
-const readline=require('readline-sync');
-const access=require('./flipCoinBL');
-let noOfTimes=readline.questionInt('How many times do you like to flip a coin :');
-console.log(access.flipCoin(noOfTimes));
+/**
+ * Execution   : default node    cmd>node.flipCoin.js
+ * 
+ * purpose     : To find whether head or tails wins
+ * 
+ * 
+ * @description :Flip Coin and print percentage of Heads and Tails
+ * @file        :flipCoin.js
+ * @overview    :coin flip is done with the help of random method and find the percentage
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :20-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./flipCoinBL');
+let noOfTimes = readline.question('How many times do you like to flip a coin :');
+if (noOfTimes === '') {
+    console.log('Give valid input');
+} else
+    console.log(access.flipCoin(noOfTimes));
--- functional/flipCoin/flipCoinBL.js
+++ functional/flipCoin/flipCoinBL.js
@@ -1,27 +1,24 @@
-
-try {
-    exports.flipCoin=(noOfTimes)=>{
-        let head=0;
-        let tail=0;
-        for(let i=0;i<noOfTimes;i++)
-        {
-            let coin1=Math.random();
-            let coin2=Math.random();
-            if(coin1>coin2)
-            {
+/**
+ * @module(number)noOfTimes-user input
+ */
+exports.flipCoin = (noOfTimes) => {
+    try {
+        let head = 0; //for validation purpose
+        let tail = 0; //for validation purpose
+        for (let i = 0; i < noOfTimes; i++) { //to loop user input times
+            let coin1 = Math.random(); //to generate random value between 0 to 1
+            let coin2 = Math.random();
+            if (coin1 > coin2) { //to check head is greater or tail is greater
                 head++;
-            }
-            else if (coin2>coin1) {
+            } else if (coin2 > coin1) {
                 tail++;
-            }
-            else
-            {
+            } else {
                 console.log('draw')
             }
         }
-        let win=head>tail?'Head wins by '+(head/noOfTimes)*100+'%':tail>head?'Tail wins by '+(tail/noOfTimes)*100+'%':'Draw';
+        let win = head > tail ? 'Head wins by ' + (head / noOfTimes) * 100 + '%' : tail > head ? 'Tail wins by ' + (tail / noOfTimes) * 100 + '%' : 'Draw'; //Checking who wins and finding percentage
         return win;
-    } 
-} catch (error) {
-    console(error);
+    } catch (error) {
+        console(error);
+    }
 }
