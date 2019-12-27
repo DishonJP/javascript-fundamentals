62c145002ade0e3172092aeb3781d2fb9bcac291
dishonjp <dishonjp11@gmail.com>
2019-12-27 11:47:48 +0530

    [Dihson]Added comments

--- logical/couponNumber/couponNumber.js
+++ logical/couponNumber/couponNumber.js
@@ -1,7 +1,20 @@
-const readline=require('readline-sync');
-const access=require('./couponNumberBL');
-let couponNumber=readline.questionInt('Enter coupon no : ');
-let arr=access.toFindCouponNumber(couponNumber)
-let set=new Set(arr);
-console.log('You won in '+set.size+1+' attempt');
-
+/**
+ * Execution   : default node    cmd>node.couponNumber.js
+ * 
+ * purpose     : To find coupon number
+ * 
+ * 
+ * @description
+ * 
+ * @file        :couponNumber.js
+ * @overview    :Generate n random numbers and find the user entered number
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :27-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./couponNumberBL');
+let couponNumber = readline.questionInt('Enter coupon no : ');
+let arr = access.toFindCouponNumber(couponNumber)
+let set = new Set(arr); //to store only unique datas
+console.log('You won in ' + set.size + 1 + ' attempt'); //to find the size of the array
--- logical/couponNumber/couponNumberBL.js
+++ logical/couponNumber/couponNumberBL.js
@@ -1,17 +1,20 @@
-exports.toFindCouponNumber=(couponNumber)=>{
-   try {
-    let arr=new Array(10000000);
-    while (true) {
-        let randomCouponNumber=Math.round(Math.random()*1000000);
-        arr.push(randomCouponNumber);
-        if (couponNumber===randomCouponNumber) 
-        {
-            break;
-        }  
+/**
+ * @module(couponNumber)//number
+ */
+exports.toFindCouponNumber = (couponNumber) => {
+    try {
+        let arr = new Array(10000000); //creating array
+        while (true) {
+            let randomCouponNumber = Math.round(Math.random() * 1000000); //Generating random numbers
+            arr.push(randomCouponNumber); //storing random value into the array
+            if (couponNumber === randomCouponNumber) // checking random number and user entered no are equal
+            {
+                break;
+            }
+        }
+        return arr;
+    } catch (error) {
+        console.log(error);
+
     }
-    return arr;
-   } catch (error) {
-       console.log(error);
-       
-   }
 }
