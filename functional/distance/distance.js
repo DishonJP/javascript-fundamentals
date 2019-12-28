a6152e9d0b28a18f50d903a7ae65672ff9ca6de7
dishonjp <dishonjp11@gmail.com>
2019-12-28 18:30:30 +0530

    [Dishon]Refactor.modified few comments, changed variable name and added conditions

--- functional/distance/distance.js
+++ functional/distance/distance.js
@@ -1,6 +1,24 @@
-const readline=require('readline-sync');
-const access=require('./distanceBL')
-let point1=readline.questionInt('Enter the value for point 1 : ');
-let point2=readline.questionInt('Enter the value for point 2 : ');
-console.log(access.findDistance(point1,point2));
+/**
+ * Execution   : default node    cmd>node.distance.js
+ * 
+ * purpose     : to find distance between two points
+ * 
+ * 
+ * @description :to calculate the distance from one point to another point
+ * @file        :distance.js
+ * @overview    :get input from the user and calculate the distance using the formula.
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :23-12-2019
+ * */
+const readline = require('readline-sync');
+const access = require('./distanceBL')
+let pointAt = readline.question('Enter the value for point 1 : ');
+let pointTo = readline.question('Enter the value for point 2 : ');
+if (pointTo === '' && pointAt === '') {
 
+} else {
+    let firstPoint = parseInt(pointAt);
+    let secondPoint = parseInt(pointTo);
+    console.log(access.findDistance(firstPoint, secondPoint));
+}
--- functional/distance/distanceBL.js
+++ functional/distance/distanceBL.js
@@ -1,9 +1,14 @@
-try {
-    exports.findDistance=(point1,point2)=>{
-        let distance=Math.sqrt(Math.pow(point1,point1)+Math.pow(point2,point2));
-        return distance;
-    }   
-} catch (error) {
-    console.log(error);
-    
+/**
+ * @module findDistance
+ * @param {number} point1 - user input
+ * @param {number} point2 - user input
+ */
+exports.findDistance = (point1, point2) => {
+    try {
+        let distance = Math.sqrt(Math.pow(point1, point1) + Math.pow(point2, point2));//to calculate distance
+        return distance;//returning value
+    } catch (error) {
+        console.log(error);
+
+    }
 }
