04210919bdf1ed841deebcda2315526701ea4c5d
dishonjp <dishonjp11@gmail.com>
2019-12-28 18:06:17 +0530

    [Dishon]Refactor.modified few comments and variable name in flip coin

--- functional/flipCoin/flipCoinBL.js
+++ functional/flipCoin/flipCoinBL.js
@@ -1,16 +1,17 @@
 /**
- * @module(number)noOfTimes-user input
+ * @module flipCoin
+ * @param {number} noOfTimes - user input
  */
 exports.flipCoin = (noOfTimes) => {
     try {
         let head = 0; //for validation purpose
         let tail = 0; //for validation purpose
         for (let i = 0; i < noOfTimes; i++) { //to loop user input times
-            let coin1 = Math.random(); //to generate random value between 0 to 1
-            let coin2 = Math.random();
-            if (coin1 > coin2) { //to check head is greater or tail is greater
+            let coinTic = Math.random(); //to generate random value between 0 to 1
+            let coinTac = Math.random();
+            if (coinTic > coinTac) { //to check head is greater or tail is greater
                 head++;
-            } else if (coin2 > coin1) {
+            } else if (coinTac > coinTic) {
                 tail++;
             } else {
                 console.log('draw')
