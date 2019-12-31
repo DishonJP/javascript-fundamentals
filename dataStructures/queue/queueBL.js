69346b61dbd53c5d63bb7c596bd0a3a721538815
dishonjp <dishonjp11@gmail.com>
2019-12-31 18:52:00 +0530

    [Dishon]added comments to queueBL in queue

--- dataStructures/queue/queueBL.js
+++ dataStructures/queue/queueBL.js
@@ -1,21 +1,27 @@
 const readline = require('readline-sync');
+/**
+ * @module queue
+ * @param {String} status - user input
+ */
 exports.queue = (status) => {
+    /**
+     * Queue.
+     */
     class Queue {
-        constructor() {
-            this.queue = [];
+        constructor() { //to create instance of Object
+            this.queue = []; //creating an empty array
         }
         enqueue(data) {
-            this.queue.push(data);
+            this.queue.push(data); //to push the data into queue
         }
         dequeue() {
-            if (this.isEmpty()) {
+            if (this.isEmpty()) { //to check whether the queue is empty
                 console.log('Queue underflow');
-
             } else {
-                return this.queue.shift();
+                return this.queue.shift(); //to delete the data at front
             }
         }
-        isEmpty() {
+        isEmpty() { //to check whether the queue is empty
             if (this.queue.length === 0) {
                 return true;
             } else {
@@ -26,10 +32,10 @@ exports.queue = (status) => {
             if (this.isEmpty()) {
                 console.log('no data is present in queue');
             } else {
-                return this.queue[0];
+                return this.queue[0]; //to find the data persent in first index
             }
         }
-        printQueue() {
+        printQueue() { //for printing purpose
             let string = '';
             let i = 0;
             while (i < this.queue.length) {
@@ -39,57 +45,71 @@ exports.queue = (status) => {
             console.log(string);
         }
     }
-    let que = new Queue();
-    let balance = 0;
+    let que = new Queue(); //creating object for Queue
+    let balance = 0; //for later purpose
     switch (status.toLowerCase()) {
         case 'withdraw':
             let withdrawAmount = readline.questionInt('Please mention the amount to withdraw : ');
-            que.enqueue(withdrawAmount);
-            withdraw(withdrawAmount);
+            que.enqueue(withdrawAmount); //pushing data into queue
+            withdraw(withdrawAmount); //calling function
             break;
         case 'deposit':
             let depositAmount = readline.questionInt('Please mention the amount to deposit : ');
-            que.enqueue(depositAmount);
-            deposit(depositAmount);
+            que.enqueue(depositAmount); //pushing data into queue
+            deposit(depositAmount); //calling function
             break;
         default:
             console.log('Give valid input');
             break;
     }
+    /**
+     * withdraw.
+     * @param	mixed	withdrawAmount
+     */
     function withdraw(withdrawAmount) {
-        if (balance >= withdrawAmount) {
-            balance = balance - withdrawAmount;
+        if (balance >= withdrawAmount) { //to check withdraw is possible
+            balance = balance - withdrawAmount; //reducing balance
             console.log('withdraw Successful, Your current balance is ' + balance);
-            userRequest();
+            userRequest(); //calling function
         } else {
             console.log('you have insufficient balance,Your balance is ' + balance);
-            userRequest();
+            userRequest(); //calling function
         }
     }
 
+    /**
+     * deposit.
+     * @param	mixed	depositAmount	
+     * @return	void
+     */
     function deposit(depositAmount) {
-        balance = balance + depositAmount;
+        balance = balance + depositAmount; //adding balance
         console.log('deposit successful your current balance is ' + balance);
-        userRequest();
+        userRequest(); //calling function
     }
-
+    /**
+     * userRequest.
+     * @return	void
+     */
     function userRequest() {
         let action = readline.question('Furthur do you like to deposit money or withdraw available balance or cancel your request :');
         switch (action.toLowerCase()) {
             case 'withdraw':
                 let withAmount = readline.questionInt('Please mention the amount to withdraw : ');
-                withdraw(withAmount);
+                withdraw(withAmount); //calling function
                 break;
             case 'deposit':
                 let depAmount = readline.questionInt('Please mention the amount to deposit : ');
-                deposit(depAmount);
+                deposit(depAmount); //calling function
                 break;
             case 'cancel':
-                que.dequeue();
+                que.dequeue(); //ending the process
                 break;
             default:
                 console.log('invalid input');
                 break;
         }
+
+
     }
 }
