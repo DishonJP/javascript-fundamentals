a2a9b812cdeed3f2d0c14e1428df5209bcd49f9b
dishonjp <dishonjp11@gmail.com>
2019-12-31 12:43:22 +0530

    [dishon]comments are added to Unordered List

--- dataStructures/unOrderedList/unOrderedList.js
+++ dataStructures/unOrderedList/unOrderedList.js
@@ -1,3 +1,16 @@
+/**
+ * Execution   : default node    cmd>node.unOrderedList.js
+ * 
+ * purpose     : add or remove a string
+ * 
+ * 
+ * @description :Read the Text from a file, split it into words and arrange it as Linked List. Take a user input to search a Word in the List. If the Word is not found then add it to the list, and if it found then remove the word from the List. In the end save the list into a file.
+ * @file        :unOrderedList.js
+ * @overview    :add and remove elements using custom methods 
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :30-12-2019
+ * */
 const access = require('./unOrderedListBL');
 let fs = require('fs');
 let content = fs.readFileSync('happen.txt', 'utf8');
--- dataStructures/unOrderedList/unOrderedListBL.js
+++ dataStructures/unOrderedList/unOrderedListBL.js
@@ -1,69 +1,73 @@
+/**
+ * @module unOrderedList
+ * @param {Array} data - list of words
+ */
 const readline = require('readline-sync');
 exports.unOrderedList = (data) => {
+    /**
+     * Node.
+     */
     class Node {
-        constructor(data) {
-            this.data = data;
-            this.next = null;
+        constructor(data) {//to create instance of an object
+            this.data = data;//for storing data
+            this.next = null;//for pointing to next node
         }
 
     }
+    /**
+     * LinkedList.
+     */
     class LinkedList {
-        constructor() {
-            this.head = null;
-            this.size = 0;
+        constructor() {//to create instance of an object
+            this.head = null;//to find which node is above all
+            this.size = 0;//for calculating size of node
 
         }
+        /**
+         * @param {any} data - user input
+         */
         add(data) {
-            let node = new Node(data);
+            let node = new Node(data);//creating instance
             let current;
-            if (this.head === null) {
-                this.head = node;
+            if (this.head === null) {//checking head is null means no data in linked list
+                this.head = node;//pointing head->node;
             } else {
-                current = this.head;
-                while (current.next !== null) {
-                    current = current.next;
+                current = this.head;//taking temp variable so that head value will not be changed
+                while (current.next !== null) {//checking head has next node 
+                    current = current.next;//changing current head from next node head
                 }
                 current.next = node;
             }
             this.size++;
         }
+        /**
+         * @param {any} data - user input
+         */
         remove(data) {
             let current = this.head;
-            let previous = null;
-            while (current !== null) {
-                if (current.data.toLowerCase() === data.toLowerCase()) {
-                    if (previous === null) {
-                        this.head = current.next;
+            let previous = null;//to store previous node value
+            while (current !== null) {//looping entire list
+                if (current.data.toLowerCase() === data.toLowerCase()) {//checking whether data is present in the list or not
+                    if (previous === null) {//means only one node is there in the list
+                        this.head = current.next;//making head=null
                     } else {
-                        previous.next = current.next;
+                        previous.next = current.next;//making previous node to next of currently dereffered node
                     }
                     this.size--;
-                    return current.data;
+                    return current.data;//retuning data from current head node
                 }
                 previous = current;
                 current = current.next;
             }
             return -1;
         }
-        pop() {
-            let current = this.head;
-            let previous = null;
-            if (current === null) {
-                return false;
-            } else {
-                while (current.next !== null) {
-                    previous = current;
-                    current = current.next;
-                }
-                current.data = null;
-                return true;
-            }
-
-        }
+        /**
+         * @param {any} data - user input
+         */
         search(data) {
             let current = this.head;
             while (current !== null) {
-                if (current.data.toLowerCase() === data.toLowerCase()) {
+                if (current.data.toLowerCase() === data.toLowerCase()) {//to check the data present in the list
                     return true;
                 }
                 current = current.next;
@@ -71,7 +75,7 @@ exports.unOrderedList = (data) => {
             return false;
 
         }
-        printList() {
+        printList() {//for displaying the data in the list
             let current = this.head;
             let string = '';
             while (current) {
@@ -80,26 +84,21 @@ exports.unOrderedList = (data) => {
             }
             console.log(string);
         }
+    
     }
-    let list = new LinkedList();
+    let list = new LinkedList();//creating object for LinkedList
     let i = 0;
-    while (i < data.length) {
+    while (i < data.length) {//add all the data from the file to the linked list
         list.add(data[i]);
         i++;
     }
     list.printList();
     let userData = readline.question('Enter a word : ');
-    if (list.search(userData)) {
-        list.remove(userData);
+    if (list.search(userData)) {//it returns true / false 
+        list.remove(userData);//if true, the data will be removed
     } else {
-        list.add(userData);
+        list.add(userData);//if false ,the data is added
     }
-    if (list.pop()) {
-        console.log('done');
+    list.printList();//just printing
 
-    } else {
-        console.log('nope');
-
-    }
-    list.printList();
 }
