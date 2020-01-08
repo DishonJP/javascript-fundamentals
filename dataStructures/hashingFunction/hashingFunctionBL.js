6b8895d3c3ffbcb3659eeba06faf7fb2a746f082
dineshkar <dineshkar08@gmail.com>
2020-01-08 12:12:59 +0530

    [Dishon].added set method to Hashing Function

--- dataStructures/hashingFunction/hashingFunctionBL.js
+++ dataStructures/hashingFunction/hashingFunctionBL.js
@@ -1,10 +1,15 @@
 exports.hashingFunction = (data) => {
-    class HashTable{
+    class HashTable {
         constructor() {
             this.bucket = new Array(11);
         }
         key(data) {
             return data % 11;
+        }
+        set(key, value) {
+            let index = this.key(key);
+            this.bucket[index].push([key, value]);
+            return index;
         }
     }
 }
