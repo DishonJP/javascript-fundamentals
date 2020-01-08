5629fe3bf7f1261937f2f30e7f1515487bd2e72a
dineshkar <dineshkar08@gmail.com>
2020-01-08 15:18:18 +0530

    [Dishon].added comments to Hashing Function

--- dataStructures/hashingFunction/hashingFunction.js
+++ dataStructures/hashingFunction/hashingFunction.js
@@ -1,3 +1,16 @@
+/**
+ * Execution   : default node    cmd>node.hashingFunction.js
+ * 
+ * purpose     : to store and fetch the no from file
+ * 
+ * 
+ * @description :Create a Slot of 10 to store Chain of Numbers that belong to each Slot to efficiently search a number from a given set of number.
+ * @file        :hashingFunction.js
+ * @overview    :using hashmap datastructure in order to complete the task
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :8-1-2020
+ * */
 const access = require('./hashingFunctionBL');
 const fs = require('fs');
 let content = fs.readFileSync('numbers.txt', 'utf8');
--- dataStructures/hashingFunction/hashingFunctionBL.js
+++ dataStructures/hashingFunction/hashingFunctionBL.js
@@ -1,24 +1,42 @@
+/**
+ * @module hashingFunction
+ * @param {number} data - data from file
+ */
 exports.hashingFunction = (data) => {
+    /**
+     * HashTable.
+     */
     class HashTable {
         constructor() {
-            this.buckets = new Array(11);
+            this.buckets = new Array(11);//creating new array
         }
+        /**
+         * @param {number} data - data from file
+         */
         hash(data) {
-            return data % 11;
+            return data % 11;//to find index
         }
+        /**
+         * 
+         * @param {number} key 
+         * @param {number} value 
+         */
         set(key, value) {
-            let index = this.hash(key);
-            this.buckets[index] = value;
+            let index = this.hash(key);//calling hash method to find index
+            this.buckets[index] = value;//storing value
         }
+        /**
+         * @param {number} key 
+         */
         get(key) {
-            const index = this.hash(key);
-            return this.buckets[index];
+            const index = this.hash(key);//calling hash method to find index
+            return this.buckets[index];//to mfind the value present in user requested index
         }
     }
-    let hashTable = new HashTable();
+    let hashTable = new HashTable();//creating instance
     let i = 0;
-    while (i < data.length) {
-        hashTable.set(data[i], data[i]);
+    while (i < data.length) {//looping the data
+        hashTable.set(data[i], data[i]);//pushing data into hash map
         i++;
     }
     console.log(hashTable.get(4));
