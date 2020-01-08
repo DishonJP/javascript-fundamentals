f2682c78939815d87cf252668c39ead5536d05d6
dineshkar <dineshkar08@gmail.com>
2020-01-08 09:54:19 +0530

    [Dishon].added comments to Binary Search Tree

--- dataStructures/binarySearchTree/binarySearchTree.js
+++ dataStructures/binarySearchTree/binarySearchTree.js
@@ -1,3 +1,16 @@
+/**
+ * Execution   : default node    cmd>node.binarySearchTree.js
+ * 
+ * purpose     : find the number of different binary search trees that can be created using these nodes.
+ * 
+ * 
+ * @description :First line will contain an integer, T, number of test cases. Then T lines follow, where each line represent a test case. Each test case consists a single integer, N, where N is the number of nodes in the binary search tree.
+ * @file        :binarySearchTree.js
+ * @overview    :add elements using custom methods 
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :30-12-2019
+ * */
 const readline = require('readline-sync');
 const access = require('./binarySearchTreeBL');
 let size = readline.question('Enter the size of array : ');
--- dataStructures/binarySearchTree/binarySearchTreeBL.js
+++ dataStructures/binarySearchTree/binarySearchTreeBL.js
@@ -1,54 +1,80 @@
 const readline = require('readline-sync');
+/**
+ * @module binarySearchTree
+ * @param {number} size - for creating array
+ */
 exports.binarySearchTree = (size) => {
     let arr = new Array(size);
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {//for storing value in index
         let value = readline.questionInt('Enter the valur for index ' + i + ' : ');
         arr[i] = value;
     }
+    /**
+     * Node.
+     */
     class Node {
         constructor(data) {
-            this.data = data;
-            this.right = null;
-            this.left = null;
+            this.data = data;//for storing data
+            this.right = null;//right tree
+            this.left = null;//left tree
         }
     }
+    /**
+     * BinarySearchTree.
+     */
     class BinarySearchTree {
         constructor() {
-            this.head = null;
+            this.head = null;//for assigning root node
         }
+        /**
+         * 
+         * @param {Any} data 
+         */
         add(data) {
+            let count = 0;
             let node = new Node(data);
             if (this.head === null) {
-                this.head = node;
+                this.head = node;//if head is null then setting head value
                 return true;
             } else {
-                let current = this.head;
+                let current = this.head;//taking tempory variable so that head value will not be changed
                 while (true) {
-                    if (current.data > data) {
-                        if (current.left !== null) {
+                    if (current.data > data) {//if data is less than current data it will be sent to left
+                        if (current.left !== null) {//we can only store data in empty node so we looping to find empty node
                             current = current.left;
                         } else {
-                            current.left = node;
+                            current.left = node;//assigning 
                             break;
                         }
-                    } else if (current.data < data) {
+                    } else if (current.data < data) {//if data is greater than current data it will be sent to right
                         if (current.right !== null) {
                             current = current.right;
                         } else {
-                            current.right = node;
+                            current.right = node;//assigning 
                             break;
                         }
                     }
                 }
             }
+        
+        
+        
+        }
+        search(data) {
+            if (this.head.data === data) {
+                return true;
+            } else {
+                let current = this.head;
+
+            }
+
         }
     }
     let bst = new BinarySearchTree();
     let i = 0;
     while (i < arr.length) {
-        bst.add(arr[i]);
+        bst.add(arr[i]);//to add the elements to binary search tree
         i++;
     }
     console.log(bst);
-
 }
