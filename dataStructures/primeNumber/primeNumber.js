dad4788f24f7392d6e3c5b193b97d910a40516b2
dineshkar <dineshkar08@gmail.com>
2020-01-08 15:08:29 +0530

    [Dishon].added comments to prime number

--- dataStructures/primeNumber/primeNumber.js
+++ dataStructures/primeNumber/primeNumber.js
@@ -1,3 +1,16 @@
+/**
+ * Execution   : default node    cmd>node.primeNumber.js
+ * 
+ * purpose     : to find prime number within range
+ * 
+ * 
+ * @description :Take a range of 0 - 1000 Numbers and find the Prime numbers in that range. Store the prime numbers in a 2D Array, the first dimension represents the range 0-100, 100-200, and so on. While the second dimension represents the prime numbers in that range.
+ * @file        :primeNumber.js
+ * @overview    :create a 2D array and store the prime no 
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :8-1-2020
+ * */
 const access = require('./primeNumberBL');
 let arr = new Array(10);
 console.log(arr);
--- dataStructures/primeNumber/primeNumberBL.js
+++ dataStructures/primeNumber/primeNumberBL.js
@@ -1,5 +1,14 @@
+/**
+ * @module toFindPrime
+ * @param {Array} arr - normal array
+ */
 exports.toFindPrime = (arr) => {
-    function isPrime(index) {
+    /**
+     * isPrime.
+     * @param	{number} index	
+     * @return	boolean
+     */
+    function isPrime(index) {// to find prime no
         let i = 2;
         while (i <= index / 2) {
             if (index % i != 0) {
@@ -7,21 +16,21 @@ exports.toFindPrime = (arr) => {
             } else
                 return false;
         }
-        return true;
+        return true;//when it is prime no it returns true
     }
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(100);
+        arr[i] = new Array(100);//creating 2D array
     }
-    let j = 0;
-    let k = 0;
-    for (let i = 2; i <= 1000; i++) {
-        if (i % 100 === 0) {
-            k = 0;
-            j++;
+    let j = 0;//helps to store in specified index
+    let k = 0;//helps to store in specified index
+    for (let i = 2; i <= 1000; i++) {//to loop till 1000
+        if (i % 100 === 0) {//in order to store only 100 range wise prime nos
+            k = 0;//to make to start from index 0
+            j++;//increment
         }
         let result = isPrime(i);
         if (result) {    
-            arr[j][k] = i;
+            arr[j][k] = i;//storing only prime no
             k++;
         }
     }
