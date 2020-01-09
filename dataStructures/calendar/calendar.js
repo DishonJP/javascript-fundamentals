20d46d7d7a550306e9053a0cdb7ae784cc608b35
dineshkar <dineshkar08@gmail.com>
2020-01-09 17:28:23 +0530

    [Dishon].added calendar to dataStructures

--- dataStructures/calendar/calendar.js
+++ dataStructures/calendar/calendar.js
@@ -2,7 +2,7 @@ const readline = require('readline-sync');
 const access = require('./calendarBL');
 let month = readline.question('Enter the month : ');
 let year = readline.question('Enter the year : ');
-if (month === '' && year === '' && (month <= 12 && month >= 1) && year.length === 4) {
+if (month === '' && year === '' && (parseInt(month) > 12 && parseInt(month) < 1) && parseInt(year).length !== 4) {
     console.log('Give valid input');
 } else {
     access.calendar(parseInt(month), parseInt(year));
--- dataStructures/calendar/calendarBL.js
+++ dataStructures/calendar/calendarBL.js
@@ -1,24 +1,40 @@
 exports.calendar = (month, year) => {
-    function date(month, year, n) {
-        return new Date(year, month, n).getDate();
+    function day(month, year, n) {
+        return new Date(year, month - 1, n).getDay();
     }
-    console.log(date);
 
-    let arr = new Array(6);
+    function date(month, year, n) {
+        return new Date(year, month - 1, n).getDate();
+    }
+    let arr = new Array(7);
     for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(7);
     }
-    arr[0][1] = 'M', arr[0][2] = 'T', arr[0][3] = 'W', arr[0][4] = 'TH', arr[0][5] = 'F', arr[0][6] = 'S', arr[0][0] = 'S';
+    let lastDate = date(month, year, 0);
+    let firstDate = date(month, year, 1);
+    arr[0][1] = 'Mon', arr[0][2] = 'Tue', arr[0][3] = 'Wed', arr[0][4] = 'THU', arr[0][5] = 'Fri', arr[0][6] = 'Sat', arr[0][0] = 'Sun';
     for (let i = 1; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-            arr[i][j] = date(month, year, 0);
+            if (j === day(month, year, firstDate) && firstDate <= lastDate) {
+                arr[i][j] = firstDate;
+                firstDate++;
+            } else {
+                arr[i][j] = '';
+            }
         }
     }
-
     for (let i = 0; i < arr.length; i++) {
         let string = '';
         for (let j = 0; j < arr.length; j++) {
-            string = string + arr[i][j] + '   ';
+            if (i === 0) {
+                string = string + arr[i][j] + ' ';
+            } else if (arr[i][j] === '') {
+                string = string + '    ';
+            } else if (arr[i][j] > 9) {
+                string = string + arr[i][j] + '  ';
+            } else {
+                string = string + arr[i][j] + '   ';
+            }
         }
         console.log(string);
 
