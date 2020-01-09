ef1799de11c0ae575250c3a68a7cba79ec589f1b
dineshkar <dineshkar08@gmail.com>
2020-01-09 15:40:21 +0530

    [Dishon]added comments to anagramStack

--- dataStructures/anagramStack/anagramStack.js
+++ dataStructures/anagramStack/anagramStack.js
@@ -1,2 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.anagramStack.js
+ * 
+ * purpose     : print anagram of prime no of range 1 t0 1000 in reverse using custom method
+ * 
+ * 
+ * @description :Add the Prime Numbers that are Anagram in the Range of 0 - 1000 in a Stack using the Linked List and Print the Anagrams in the Reverse Order.
+ * @file        :anagramStack.js
+ * @overview    :print anagram of prime no of range 1 t0 1000 in reverse using custom method
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :8-1-2020
+ * */
 const access = require('./anagramStackBL');
 access.linkedList();
