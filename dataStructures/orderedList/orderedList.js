fab695563271e37a531cbe07134686e55c2a440a
dineshkar <dineshkar08@gmail.com>
2020-01-10 12:41:29 +0530

    [Dishon].added comments to OrderedList

--- dataStructures/orderedList/orderedList.js
+++ dataStructures/orderedList/orderedList.js
@@ -1,3 +1,16 @@
+/**
+ * Execution   : default node    cmd>node.orderedList.js
+ * 
+ * purpose     : add or remove a user number
+ * 
+ * 
+ * @description :Read .a List of Numbers from a file and arrange it ascending Order in a Linked List. Take user input for a number, if found then pop the number out of the list else insert the number in appropriate position.
+ * @file        :orderedList.js
+ * @overview    :add and remove elements using custom methods 
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :30-12-2019
+ * */
 const fs = require('fs');
 const access = require('./orderedListBL');
 let content = fs.readFileSync('orderedList.txt', 'utf8');
--- dataStructures/orderedList/orderedListBL.js
+++ dataStructures/orderedList/orderedListBL.js
@@ -1,32 +1,54 @@
 const readline = require('readline-sync');
+/**
+ * @module orderedList
+ * @param {number} data
+ */
 exports.orderedList = (data) => {
+    /**
+     * Node.
+     */
     class Node {
+        /**
+         * 
+         * @param {number} data 
+         */
         constructor(data) {
             this.data = data;
             this.next = null;
         }
     }
+    /**
+     * OrderedList.
+     */
     class OrderedList {
         constructor() {
             this.head = null;
         }
+        /**
+         * 
+         * @param {number} data 
+         */
         add(data) {
             let node = new Node(data);
 
             if (this.head === null) {
-                this.head = node;
+                this.head = node; //adding data to list
             } else {
                 let current = this.head;
-                while (current.next !== null) {
+                while (current.next !== null) { //helps to find the empty node
                     current = current.next;
                 }
-                current.next = node;
+                current.next = node; //adding data to list
             }
         }
+        /**
+         * 
+         * @param {number} data 
+         */
         search(data) {
             let current = this.head;
             while (current.next !== null) {
-                if (current.data == data) {
+                if (current.data == data) { //helps to find whether the data is present in the list
                     return true;
                 }
                 current = current.next;
@@ -36,27 +58,31 @@ exports.orderedList = (data) => {
             }
             return false;
         }
+        /**
+         * 
+         * @param {number} data 
+         */
         pop(data) {
             if (this.head.data == data) {
                 this.head = this.head.next;
             } else {
                 let current = this.head;
                 let prev = null;
-                while (current.data !== data) {
+                while (current.data !== data) { //finding the location of data
                     prev = current;
                     current = current.next;
                 }
-                prev.next = current.next;
+                prev.next = current.next; //derefering
             }
         }
         display() {
             let arr = [];
             let current = this.head;
             while (current.next !== null) {
-                arr.push(parseInt(current.data));
+                arr.push(parseInt(current.data)); //converting string to number and pushing to array
                 current = current.next;
             }
-            myFunction();
+            myFunction(); //for sorting the data
             function myFunction() {
                 arr.sort(function (a, b) {
                     return a - b
@@ -65,19 +91,18 @@ exports.orderedList = (data) => {
             console.log(arr);
         }
     }
-    let list = new OrderedList();
-    data.sort();
+    let list = new OrderedList(); //creating object
     for (let i = 0; i < data.length; i++) {
-        list.add(data[i]);
+        list.add(data[i]); //adding data to list
     }
     let userData = readline.questionInt('Enter a number : ');
-    let result = list.search(userData);
-    if (result) {
+    let result = list.search(userData); //calling custom search method of list
+    if (result) { //to check the data is present in the list
         list.pop(userData);
         console.log('removed');
     } else {
         list.add(userData);
         console.log('added');
     }
-    list.display();
+    list.display(); //calling custom display method
 }
