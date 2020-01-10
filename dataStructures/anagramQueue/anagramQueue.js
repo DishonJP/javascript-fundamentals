a81224a0ff94448a4b67edd1bebc2fb61e443a4a
dineshkar <dineshkar08@gmail.com>
2020-01-10 10:26:59 +0530

    [Dishon].added comments AnagramQueue

--- dataStructures/anagramQueue/anagramQueue.js
+++ dataStructures/anagramQueue/anagramQueue.js
@@ -1,2 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.anagramQueue.js
+ * 
+ * purpose     : print anagram of prime no of range 1 t0 1000 using custom method
+ * 
+ * 
+ * @description :Add the Prime Numbers that are Anagram in the Range of 0 - 1000 in a Queue using the Linked List and Print the Anagrams from the Queue.
+ * @file        :anagramQueue.js
+ * @overview    :print anagram of prime no of range 1 t0 1000 using custom method
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :10-1-2020
+ * */
 const access = require('./anagramQueueBL');
 access.linkedList();
