27d1f1056d009b6b11dffe0c276ee1288a9fbcca
dineshkar <dineshkar08@gmail.com>
2020-01-10 12:30:18 +0530

    [Dishon].added OrderedList to dataStructures

--- dataStructures/orderedList/orderedListBL.js
+++ dataStructures/orderedList/orderedListBL.js
@@ -1,3 +1,4 @@
+const readline = require('readline-sync');
 exports.orderedList = (data) => {
     class Node {
         constructor(data) {
@@ -16,21 +17,67 @@ exports.orderedList = (data) => {
                 this.head = node;
             } else {
                 let current = this.head;
-                let previous = null;
                 while (current.next !== null) {
-                    if (current.data>data) {
-                        
-                    }
                     current = current.next;
                 }
                 current.next = node;
             }
         }
+        search(data) {
+            let current = this.head;
+            while (current.next !== null) {
+                if (current.data == data) {
+                    return true;
+                }
+                current = current.next;
+            }
+            if (current.data == data) {
+                return true;
+            }
+            return false;
+        }
+        pop(data) {
+            if (this.head.data == data) {
+                this.head = this.head.next;
+            } else {
+                let current = this.head;
+                let prev = null;
+                while (current.data !== data) {
+                    prev = current;
+                    current = current.next;
+                }
+                prev.next = current.next;
+            }
+        }
+        display() {
+            let arr = [];
+            let current = this.head;
+            while (current.next !== null) {
+                arr.push(parseInt(current.data));
+                current = current.next;
+            }
+            myFunction();
+            function myFunction() {
+                arr.sort(function (a, b) {
+                    return a - b
+                });
+            }
+            console.log(arr);
+        }
     }
     let list = new OrderedList();
+    data.sort();
     for (let i = 0; i < data.length; i++) {
         list.add(data[i]);
     }
-    console.log(list);
-
+    let userData = readline.questionInt('Enter a number : ');
+    let result = list.search(userData);
+    if (result) {
+        list.pop(userData);
+        console.log('removed');
+    } else {
+        list.add(userData);
+        console.log('added');
+    }
+    list.display();
 }
