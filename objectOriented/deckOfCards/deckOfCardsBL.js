879bc9292dc912c3d3d8f925728bcccb1c3ae04f
dineshkar <dineshkar08@gmail.com>
2020-01-11 15:48:19 +0530

    [Dishon].added deckOfCards to ObjectOriented

--- objectOriented/deckOfCards/deckOfCardsBL.js
+++ objectOriented/deckOfCards/deckOfCardsBL.js
@@ -1,6 +1,3 @@
-const uniqueRandomArray = require('unique-random-array');
-const uniqueRandom = require('unique-random');
-const randomInt = require('random-int');
 exports.deckOfCards = () => {
     let cards = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
     for (let i = 0; i < cards.length; i++) {
@@ -22,20 +19,86 @@ exports.deckOfCards = () => {
             if (i === 3) {
                 deckOfCards.push('Spade ' + cards[i][j]);
             }
-
         }
     }
-    console.log(deckOfCards);
-    let arr = new Set([]);
+    let arr = [];
     let i = 0;
     while (true) {
-        let randomValue = Math.floor(Math.random() * 52 + 1);
-        arr.add(1, randomValue);
-        i++;
-        console.log(arr);
+        let randomValue = Math.floor(Math.random() * 52);
+        let result = checkingDuplicate(randomValue);
+        if (result) {
+            arr.push(randomValue);
+        }
         if (arr.length === 52) {
             break;
         }
     }
-    console.log(arr);
+
+    function checkingDuplicate(i) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i === arr[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    let players = [
+        [],
+        [],
+        [],
+        []
+    ];
+    let k = 0;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 9; j++) {
+            players[i][j] = deckOfCards[arr[k]];
+            k++;
+        }
+    }
+
+    function display(players) {
+        let string = '';
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 9; j++) {
+
+                if (i === 0) {
+                    if (j === 0) {
+                        console.log('player ' + (i + 1) + ' cards\n');
+                    }
+                    console.log(players[i][j]);
+                    if (j === 8) {
+                        console.log();
+                    }
+                }
+                if (i === 1) {
+                    if (j === 0) {
+                        console.log('player ' + (i + 1) + ' cards\n');
+                    }
+                    console.log(players[i][j]);
+                    if (j === 8) {
+                        console.log();
+                    }
+                }
+                if (i === 2) {
+                    if (j === 0) {
+                        console.log('player ' + (i + 1) + ' cards\n');
+                    }
+                    console.log(players[i][j]);
+                    if (j === 8) {
+                        console.log();
+                    }
+                }
+                if (i === 3) {
+                    if (j === 0) {
+                        console.log('player ' + (i + 1) + ' cards\n');
+                    }
+                    console.log(players[i][j]);
+                    if (j === 8) {
+                        console.log();
+                    }
+                }
+            }
+        }
+    }
+    display(players);
 }
