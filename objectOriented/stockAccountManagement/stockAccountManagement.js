4d59c26373e0b34a21431c59b5b3b1738ebb8733
dineshkar <dineshkar08@gmail.com>
2020-01-13 12:40:36 +0530

    [Dishon].added comments to stockAccountManagement

--- objectOriented/stockAccountManagement/stockAccountManagement.js
+++ objectOriented/stockAccountManagement/stockAccountManagement.js
@@ -1,3 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.stockAccountManagement.js
+ * 
+ * purpose     : to find total share of each stock and total price of all the stock
+ * 
+ * @description :Write a program to read in Stock Names, Number of Share, Share Price. Print a Stock Report with total value of each Stock and the total value of Stock.
+ * @file        :stockAccountManagement.js
+ * @overview    :using json to retrive data and using several condtion to display the data
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :13-1-2020
+ * */
 const access = require('./stockAccountManagementBL');
 const fs = require('fs');
 let pkg = JSON.parse(fs.readFileSync('./stock.json'));
--- objectOriented/stockAccountManagement/stockAccountManagementBL.js
+++ objectOriented/stockAccountManagement/stockAccountManagementBL.js
@@ -1,6 +1,18 @@
-const readline = require('readline-sync');
+/**
+ * @module stockAccountManagement
+ * @param {mixed} data - data from file
+ */
 exports.stockAccountManagement = (data) => {
+    /**
+     * .StockAccountManagement
+     */
     class StockAccountManagement {
+        /**
+         * 
+         * @param {string} stockName 
+         * @param {number} noOfShare 
+         * @param {number} sharePrice 
+         */
         constructor(stockName, noOfShare, sharePrice) {
             this.stockName = stockName;
             this.sharePrice = sharePrice;
@@ -8,19 +20,18 @@ exports.stockAccountManagement = (data) => {
             this.totalShare = sharePrice * noOfShare;
         }
     }
-    let allStock = [];
+    let allStock = []; //creating empty array
     let i = 0;
-    while (i < data.stocks.length) {
-        let stock = new StockAccountManagement(data.stocks[i].stockName, data.stocks[i].noOfShares, data.stocks[i].stockPrice);
-        allStock.push(stock);
+    while (i < data.stocks.length) { //to loop the data present in a file
+        let stock = new StockAccountManagement(data.stocks[i].stockName, data.stocks[i].noOfShares, data.stocks[i].stockPrice); //creating new object
+        allStock.push(stock); //pushing data into array
         i++;
     }
     console.log(allStock);
     let j = 0;
-    let grandTotal = 0;
+    let grandTotal = 0; //to find grand total
     while (j < allStock.length) {
-        console.log(allStock[j].totalShare);
-        grandTotal += allStock[j].totalShare;
+        grandTotal += allStock[j].totalShare; //to find grand total
         j++;
     }
     console.log('Grand Total for stock is ' + grandTotal);
