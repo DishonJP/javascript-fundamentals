60867007f6155fd7c34d2e7a57b1e612fe33e2c9
dineshkar <dineshkar08@gmail.com>
2020-01-13 15:09:03 +0530

    [Dishon].added comments to dataManagement

--- objectOriented/dataManagement/dataManagement.js
+++ objectOriented/dataManagement/dataManagement.js
@@ -1,3 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.dataManagement.js
+ * 
+ * purpose     : to find total cost for the inventory
+ * 
+ * @description :Create a JSON file having Inventory Details for Rice, Pulses and Wheats with properties name, weight, price per kg.
+ * @file        :dataManagement.js
+ * @overview    :retriving data from json file and finding the amount
+ * @author      :Dishon <dishonjp11@gmail.com>
+ * @version     :1.0
+ * @since       :13-1-2020
+ * */
 const access = require('./dataManagementBL');
 const fs = require('fs');
 let data = JSON.parse(fs.readFileSync('./details.json'));
